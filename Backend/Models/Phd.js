ab47ed39ea2da434d10a4ce19b4ad1f518c7733e
Pavas Garg <pavasgarg2003@gmail.com>
2022-11-23 19:14:42 +0530

    saving phd details to db and extracting email

--- Backend/Models/Phd.js
+++ Backend/Models/Phd.js
@@ -3,44 +3,33 @@ const mongoose = require("mongoose");
 const phdSchema = new mongoose.Schema({
     name:{
         type:String,
-        required:true,
+        // required:true,
     },
     department:{
         type:String,
-        required:true,
-    },
-    sex:{
-        type:String,
-        required:true,
-    },
-    Category:{
-        type:String,
-        required:true,
+        // required:true,
     },
     id:{
-        type:Number,
-        required:true,
+        type:String,
+        // required:true,
     },
     campusId:{
         type:String,
-        required:true,
-    },
-    sourceOfFellowship:{
-        type:String,
-        required:true,
+        // required:true,
     },
     phoneNo:{
         type:Number,
-        required:true,
+        // required:true,
     },
     emailId:{
         type:String,
-        required:true,
+        // required:true,
     },
     leave:{
         type:Boolean,
     }
 });
 
-module.exports = mongoose.model("phd",phdSchema);
+const Phd = mongoose.model("phd",phdSchema);
+module.exports = Phd;
 
--- Backend/index.js
+++ Backend/index.js
@@ -7,69 +7,97 @@ const Hod = require("../Backend/Models/Hod");
 const Phd = require("../Backend/Models/Phd");
 const hbs = require("nodemailer-express-handlebars");
 const path = require("path");
+const connectToMongo = require("../Backend/db");
+connectToMongo();
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+// submit endpoint when user fills the form for leave
 app.post("/submit",async (req,res)=>{
+    try{
 
-    let transporter = nodemailer.createTransport({
-        service:"gmail",
-        auth:{
-            user:"",
-            pass:""
-        },
-        tls:{
-            rejectUnauthorized:false,
-        }
-    });
+        var id = req.query.id;
+        var name = req.query.name;
+        var email = req.query.email;
+        var batch = req.query.batch;
 
-    const handlebarOptions = {
-        viewEngine:{
+        let transporter = nodemailer.createTransport({
+            service:"gmail",
+            auth:{
+                user:"pavasgarg2003@gmail.com",
+                pass:"fiqtvhqswyhjwhtn"
+            },
+            tls:{
+                rejectUnauthorized:false,
+            }
+        });
+
+        const handlebarOptions = {
+            viewEngine:{
+                extName:".handlebars",
+                partialsDir:path.resolve("./view"),
+                defaultLayout:false,
+            },
+            viewPath:path.resolve("./view"),
             extName:".handlebars",
-            partialsDir:path.resolve("./view"),
-            defaultLayout:false,
-        },
-        viewPath:path.resolve("./view"),
-        extName:".handlebars",
-    }
-
-    transporter.use('compile',hbs(handlebarOptions));
-    
-    // let hod = await Hod.findOne({department:req.body.branch});
-    // let hodEmail = hod.email;
-
-    let mailOptions = {
-        from:"",
-        to:"",
-        subject:"PhD leave portal",
-        context:{
-            title:"Request for leave",
-            id:req.body.id,
-            text:`Respected Sir,
-            I am ${req.body.name} from ${req.body.batch} batch. Please grant me a leave as ${req.body.reason}. My id number is ${req.body.id} and my branch is ${req.body.branch}`,
-        },
-        template:'index',
-    }
-
-    transporter.sendMail(mailOptions,(err,success)=>{
-        if(err){
-            console.log(err);
         }
-        console.log("Email sent successfully!!");
-    })
-    res.redirect("http://localhost:3000");
+
+        transporter.use('compile',hbs(handlebarOptions));
+
+        // will find the email of the hod to whom the mail will be sent
+        // let hod = await Hod.findOne({department:req.body.branch});
+        // let hodEmail = hod.email;
+
+        let mailOptions = {
+            from:"pavasgarg2003@gmail.com",
+            to:"pavas23@gmail.com",
+            subject:"PhD leave portal",
+            context:{
+                title:"Request for leave",
+                id:id,
+                text:`Respected Sir,
+                I am ${name} from ${batch} batch. Please grant me a leave as ${req.body.reason}. My id number is ${id} and my branch is ${req.body.branch}`,
+            },
+            template:'index',
+        }
+
+        transporter.sendMail(mailOptions,(err,success)=>{
+            if(err){
+                console.log(err);
+            }
+            console.log("Email sent successfully!!");
+        });
+
+        let phD = await Phd.create({
+            name:name,
+            department:req.body.branch,
+            id:id,
+            campusId:"42131234",
+            phoneNo:1234567890,
+            emailId:email,
+            leave:false,
+        });
+
+        res.redirect("http://localhost:3000");
+        
+    }catch(error){
+        console.log(error);
+        res.send("Internal Server Error Occured")
+    }
 });
 
-app.post("/reply:idPhd",async (req,res)=>{
-    let phd = await Phd.findOne({id:idPhd});
+// reply endpoint when the hod clicks the button
+app.post("/reply/",async (req,res)=>{
+    // let phd = await Phd.findOne({id:req.params.id});
     if(req.body.accept){
-        phd.leave = true
+        // phd.leave = true;
+        res.send("Accepted!!");
     }
     else{
-        phd.leave = false
+        // phd.leave = false;
+        res.send("rejected");
     }
-    res.send("response submitted");
 });
 
 app.get("/",(req,res)=>{
