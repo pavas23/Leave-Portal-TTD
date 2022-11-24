87e94c8ae848af14a2c2fc5c3ebb8e18fc0552a1
Pavas Garg <pavasgarg2003@gmail.com>
2022-11-25 02:06:21 +0530

    updated backend

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/TDLeave";
+require('dotenv').config()
+
+const mongoURI = process.env.MONGO_URI
+
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI,()=>{
--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const Hod = require("../Backend/Models/Hod");
@@ -10,117 +10,135 @@ const path = require("path");
 const cors = require("cors");
 const connectToMongo = require("../Backend/db");
 connectToMongo();
+require('dotenv').config()
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // submit endpoint when user fills the form for leave
-app.post("/submit",async (req,res)=>{
-    try{
+app.post("/submit", async (req, res) => {
+  try {
+    const { id, name, email, branch, reason, multipleDate } = req.body;
+    let newDates = [];
+    for (let date of multipleDate) {
+      const today = new Date(date);
+      const yyyy = today.getFullYear();
+      let mm = today.getMonth() + 1; // Months start at 0!
+      let dd = today.getDate();
+      if (dd < 10) dd = "0" + dd;
+      if (mm < 10) mm = "0" + mm;
 
-        var id = req.body.id;
-        var name = req.body.name;
-        var email = req.body.email;
-        var branch = req.body.branch;
-
-        let transporter = nodemailer.createTransport({
-            service:"gmail",
-            auth:{
-                user:"",
-                pass:""
-            },
-            tls:{
-                rejectUnauthorized:false,
-            }
-        });
-
-        const handlebarOptions = {
-            viewEngine:{
-                extName:".handlebars",
-                partialsDir:path.resolve("./view"),
-                defaultLayout:false,
-            },
-            viewPath:path.resolve("./view"),
-            extName:".handlebars",
-        }
-
-        transporter.use('compile',hbs(handlebarOptions));
-
-        let hod = await Hod.findOne({department:branch});
-        console.log(hod);
-        let hodEmail = hod.email;
-
-
-        let mailOptions = {
-            from:"",
-            to:hodEmail,
-            subject:"PhD leave portal",
-            context:{
-                title:"Request for leave",
-                email:email,
-                text:`Respected Sir,
-                I am ${name} . Please grant me a leave as ${req.body.reason}. My id number is ${id} and my branch is ${branch}. I want leaves for ${req.body.multipleDate}`,
-            },
-            template:'index',
-        }
-
-        transporter.sendMail(mailOptions,(err,success)=>{
-            if(err){
-                console.log(err);
-            }
-            console.log("Email sent successfully!!");
-        });
-
-        let phD = await Phd.create({
-            name:name,
-            department:branch,
-            id:id,
-            campusId:"42131234",
-            phoneNo:1234567890,
-            emailId:email,
-            leave:false,
-            reason:req.body.reason,
-            date:req.body.multipleDate,
-        });
-        res.redirect("http://localhost:3000");
-
-    }catch(error){
-        console.log(error);
-        res.send("Internal Server Error Occured")
+      const formattedToday =
+        dd.toString() + "/" + mm.toString() + "/" + yyyy.toString();
+      newDates.push(formattedToday);
     }
+    console.log(newDates);
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    const handlebarOptions = {
+      viewEngine: {
+        extName: ".handlebars",
+        partialsDir: path.resolve("./view"),
+        defaultLayout: false,
+      },
+      viewPath: path.resolve("./view"),
+      extName: ".handlebars",
+    };
+    transporter.use("compile", hbs(handlebarOptions));
+
+    let hod = await Hod.findOne({ department: branch });
+    console.log(hod);
+
+    let hodEmail = hod.email ? hod.email : 'td@hyderabad.bits-pilani.ac.in';
+
+    let mailOptions = {
+      from: "",
+      to: hodEmail,
+      subject: "Invigilation Leave Portal",
+      context: {
+        title: "Request for Invigilation Leave",
+        email: email,
+        name,
+        branch,
+        reason,
+        newDates,
+        url: process.env.BASEURL
+      },
+      template: "index",
+    };
+
+    transporter.sendMail(mailOptions, (err, success) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log("Email sent successfully!!");
+    });
+
+    let phD = await Phd.create({
+      name: name,
+      department: branch,
+      id: id,
+      campusId: "42131234",
+      phoneNo: 1234567890,
+      emailId: email,
+      leave: false,
+      reason: req.body.reason,
+      date: req.body.multipleDate,
+    });
+    res.redirect("http://localhost:3000");
+  } catch (error) {
+    console.log(error);
+    res.send("Internal Server Error Occured");
+  }
 });
 
 // reply endpoint when the hod clicks the button
-app.post("/reply",async (req,res)=>{
-    var email = req.query.email;
-    if(req.body.accept){
-        Phd.findOneAndUpdate({emailId:email},{$set:{leave:true}},(err,data)=>{
-            if(err){
-                console.log(err);
-                return;
-            }
-        });
-        res.send("Accepted!!");
-    }
-    else{
-        Phd.findOneAndUpdate({emailId:email},{$set:{leave:false}},(err,data)=>{
-            if(err){
-                console.log(err);
-                return;
-            }
-        });
-        res.send("Rejected");
-    }
+app.post("/reply", async (req, res) => {
+  var email = req.query.email;
+  if (req.body.accept) {
+    Phd.findOneAndUpdate(
+      { emailId: email },
+      { $set: { leave: true } },
+      (err, data) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+      }
+    );
+    res.send("You Have Accepted the leave. If you want to modify your request, you can hit reject/accept in the same mail again");
+  } else {
+    Phd.findOneAndUpdate(
+      { emailId: email },
+      { $set: { leave: false } },
+      (err, data) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+      }
+    );
+    res.send("You Have Rejected the leave. If you want to modify your request, you can hit reject/accept in the same mail again");
+  }
 });
 
-app.get("/",(req,res)=>{
-    res.send("Hello world");
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
 
-app.listen(PORT,(err)=>{
-    if(err){
-        console.log(err);
-        return;
-    }
-    console.log(`Express App started successfully at port number:${PORT}`);
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(`Express App started successfully at port number:${PORT}`);
 });
